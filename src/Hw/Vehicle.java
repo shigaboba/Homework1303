7d19cb9a60a16a850225b56996fd948ab8243e96
shigaboba <shigapovalrm@gmail.com>
2023-03-13 22:17:50 +0300

    HW is done

---
+++ src/Hw/Vehicle.java
@@ -0,0 +1,32 @@
+package Hw;
+
+public abstract class Vehicle {
+
+    private String modelName;
+    private int wheelsCount;
+
+    public Vehicle (String modelName, int wheelsCount){
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
